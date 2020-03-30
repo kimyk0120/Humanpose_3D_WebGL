3c02b5db19e8169a9949870bf8c14b50434b4b7c
kimyk0120 <kimyk0120@hotmail.com>
2020-03-30 20:36:03 +0900

    - 초기세팅 모듈화 - 안쓰는 리소스 파일 제거

--- js/WebGL.js
+++ js/WebGL.js
@@ -96,3 +96,14 @@ var WEBGL = {
 };
 
 // export { WEBGL };
+
+let webglBrowserCheck = function(){
+	if ( WEBGL.isWebGLAvailable() ) {
+		console.log('이 브라우저는 WEBGL을 지원합니다.');
+		return true;
+	} else {
+		console.log('이 브라우저는 WEBGL을 지원하지 않습니다.');
+		console.log(WEBGL.getWebGLErrorMessage());
+		return false;
+	}
+};
